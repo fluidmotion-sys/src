8c01548eaf58f8805db33f01e7eaebdeceb2a58a
fluidmotion-sys <julian@kiesenhofer.net>
2024-07-27 23:55:33 +0200

    Add files via upload

---
+++ convexFlow.java
@@ -0,0 +1,32 @@
+/* ----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+                            |
+         FM-VABE            |  Fluidmotion: Thermoelectric Control Engineering
+           400              |  www.fluidmotion.at
+                            |
+-------------------------------------------------------------------------------
+   Copyright (C) 2024 Fluidmotion Mechatronics GmbH
+-------------------------------------------------------------------------------
+-----------------------------------------------------------------------------*/
+import java.util.Arrays;
+public class convexFlow implements signal {
+  public convexFlow (){}
+  int[] array = {294, 292, 293, 291, 295};
+  int min = Arrays.stream(array).min().getAsInt();
+  int max = Arrays.stream(array).max().getAsInt();
+  int f = max-min;
+  public void contact()
+  {
+    int j = array.length;
+    for (int i = 0; i < j; i++) {
+    int predictor = array[i];
+    System.out.println(predictor);
+    }
+    System.out.println("*"+f+"*");
+    for (int i = 0; i < j; i++) {
+    Arrays.sort(array);
+    int increment = array[i];
+    System.out.println(increment);
+    }
+  }
+}
